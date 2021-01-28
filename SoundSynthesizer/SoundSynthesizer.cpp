96bf1e7f255320d10dc7ded72a8ddb4c03df27d4
Peixiao Zhao <u6935249@anu.edu.au>
2021-01-28 22:50:41 +0800

    2020.1.28

--- SoundSynthesizer/SoundSynthesizer.cpp
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -3,76 +3,211 @@ using namespace std;
 
 #include"olcNoiseMaker.h"
 
+// Converts frequency (Hz) to angular velocity
+double w(double dHertz)
+{
+	return dHertz * 2.0 * PI;
+}
+
+// General purpose oscillator
+#define OSC_SINE 0
+#define OSC_SQUARE 1
+#define OSC_TRIANGLE 2
+#define OSC_SAW_ANA 3
+#define OSC_SAW_DIG 4
+#define OSC_NOISE 5
+
+double osc(double dHertz, double dTime, int nType = OSC_SINE)
+{
+	switch (nType)
+	{
+	case OSC_SINE: // Sine wave bewteen -1 and +1
+		return sin(w(dHertz) * dTime);
+
+	case OSC_SQUARE: // Square wave between -1 and +1
+		return sin(w(dHertz) * dTime) > 0 ? 1.0 : -1.0;
+
+	case OSC_TRIANGLE: // Triangle wave between -1 and +1
+		return asin(sin(w(dHertz) * dTime)) * (2.0 / PI);
+
+	case OSC_SAW_ANA: // Saw wave (analogue / warm / slow)
+	{
+		double dOutput = 0.0;
+
+		for (double n = 1.0; n < 40.0; n++)
+			dOutput += (sin(n * w(dHertz) * dTime)) / n;
+
+		return dOutput * (2.0 / PI);
+	}
+
+	case OSC_SAW_DIG: // Saw Wave (optimised / harsh / fast)
+		return (2.0 / PI) * (dHertz * PI * fmod(dTime, 1.0 / dHertz) - (PI / 2.0));
+
+	case OSC_NOISE: // Pseudorandom noise
+		return 2.0 * ((double)rand() / (double)RAND_MAX) - 1.0;
+
+	default:
+		return 0.0;
+	}
+}
+
+struct sEnvelopeADSR
+{
+	double dAttackTime;
+	double dDecayTime;
+	double dSustainAmplitude;
+	double dReleaseTime;
+	double dStartAmplitude;
+	double dTriggerOffTime;
+	double dTriggerOnTime;
+	bool bNoteOn;
+
+	sEnvelopeADSR()
+	{
+		dAttackTime = 0.10;
+		dDecayTime = 0.01;
+		dStartAmplitude = 1.0;
+		dSustainAmplitude = 0.8;
+		dReleaseTime = 0.20;
+		bNoteOn = false;
+		dTriggerOffTime = 0.0;
+		dTriggerOnTime = 0.0;
+	}
+
+	double GetAmplitude(double dTime)
+	{
+		double dAmplitude = 0.0;
+		double dLifeTime = dTime - dTriggerOnTime;
+
+		if (bNoteOn)
+		{
+			// ADS
+
+			// Attack
+			if (dLifeTime <= dAttackTime)
+			{
+				dAmplitude = (dLifeTime / dAttackTime) * dStartAmplitude;
+			}
+
+			// Decay
+			if (dLifeTime > dAttackTime && dLifeTime <= (dAttackTime + dDecayTime))
+			{
+				dAmplitude = ((dLifeTime - dAttackTime) / dDecayTime) * (dSustainAmplitude - dStartAmplitude) + dStartAmplitude;
+			}
+
+			// Sustain
+			if (dLifeTime > (dAttackTime + dDecayTime))
+			{
+				// In sustain phase - dont change until note released
+				dAmplitude = dSustainAmplitude;
+			}
+		}
+		else
+		{
+			// R
+			// Note has been released, so in release phase
+			dAmplitude = ((dTime - dTriggerOffTime) / dReleaseTime) * (0.0 - dSustainAmplitude) + dSustainAmplitude;
+
+			// Amplitude should not be negative
+			if (dAmplitude <= 0.0001)
+				dAmplitude = 0.0;
+
+			return dAmplitude;
+		}
+		return dAmplitude;
+	}
+
+	void NoteOn(double dTimeOn)
+	{
+		dTriggerOnTime = dTimeOn;
+		bNoteOn = true;
+	}
+
+	void NoteOff(double dTimeOff)
+	{
+		dTriggerOffTime = dTimeOff;
+		bNoteOn = false;
+	}
+};
+
 // Global synthesizer variables
 atomic<double> dFrequencyOutput = 0.0;			// dominant output frequency of instrument, i.e. the note
+sEnvelopeADSR envelope;
 double dOctaveBaseFrequency = 110.0; // A2		// frequency of octave represented by keyboard
 double d12thRootOf2 = pow(2.0, 1.0 / 12.0);		// assuming western 12 notes per ocatve
 
-
 double MakeNoise(double dTime)
 {
-    double dOutput = sin(dFrequencyOutput * 2.0 * 3.14159 * dTime);
-    return dOutput * 0.5;       // Master Volume
+	double dOutput = envelope.GetAmplitude(dTime) *
+		(
+			+1.0 * osc(dFrequencyOutput * 0.5, dTime, OSC_SINE)
+			+ 1.0 * osc(dFrequencyOutput, dTime, OSC_SAW_ANA)
+		);
+	return dOutput * 0.5;       // Master Volume
 }
 
 int main()
 {
-    // Get all sound hardware
-    vector<wstring> devices = olcNoiseMaker<short>::Enumerate();
+	// Shameless self-promotion
+	wcout << "Multiple Oscillators with Single Amplitude Envelope, No Polyphony" << endl << endl;
 
-    // Display findings
-    //for (auto d : devices)
-    //{
-    //    wcout << "Found Output Device: " << d << endl;
-    //}
+	// Get all sound hardware
+	vector<wstring> devices = olcNoiseMaker<short>::Enumerate();
 
-    // Display a keyboard
-    wcout << endl <<
-        "|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |" << endl <<
-        "|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |" << endl <<
-        "|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__" << endl <<
-        "|     |     |     |     |     |     |     |     |     |     |" << endl <<
-        "|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
-        "|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
+	// Display findings
+	//for (auto d : devices)
+	//{
+	//	wcout << "Found Output Device: " << d << endl;
+	//}
+	wcout << "Using Device: " << devices[0] << endl;
 
+	// Display a keyboard
+	wcout << endl <<
+		"|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |" << endl <<
+		"|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |" << endl <<
+		"|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__" << endl <<
+		"|     |     |     |     |     |     |     |     |     |     |" << endl <<
+		"|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
+		"|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
 
-    // Create sound machine!!
-    olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+	// Create sound machine!!
+	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
 
-    // Link noise function with sound machine
-    sound.SetUserFunction(MakeNoise);
+	// Link noise function with sound machine
+	sound.SetUserFunction(MakeNoise);
 
-    // Sit in loop, capturing keyboard state changes and modify
-    // synthesizer output accordingly
-    int nCurrentKey = -1;
-    bool bKeyPressed = false;
-    while (1)
-    {
-        bKeyPressed = false;
-        for (int k = 0; k < 16; k++)
-        {
-            if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000)
-            {
-                if (nCurrentKey != k)
-                {
-                    dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
-                    wcout << "\rNote On : " << sound.GetTime() << "s " << dFrequencyOutput << "Hz";
-                    nCurrentKey = k;
-                }
+	// Sit in loop, capturing keyboard state changes and modify
+	// synthesizer output accordingly
+	int nCurrentKey = -1;
+	bool bKeyPressed = false;
+	while (1)
+	{
+		bKeyPressed = false;
+		for (int k = 0; k < 16; k++)
+		{
+			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000)
+			{
+				if (nCurrentKey != k)
+				{
+					dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+					envelope.NoteOn(sound.GetTime());
+					wcout << "\rNote On : " << sound.GetTime() << "s " << dFrequencyOutput << "Hz";
+					nCurrentKey = k;
+				}
+				bKeyPressed = true;
+			}
+		}
 
-                bKeyPressed = true;
-            }
-        }
+		if (!bKeyPressed)
+		{
+			if (nCurrentKey != -1)
+			{
+				wcout << "\rNote Off: " << sound.GetTime() << "s                        ";
+				envelope.NoteOff(sound.GetTime());
+				nCurrentKey = -1;
+			}
+		}
+	}
 
-        if (!bKeyPressed)
-        {
-            if (nCurrentKey != -1)
-            {
-                wcout << "\rNote Off: " << sound.GetTime() << "s                        ";
-                nCurrentKey = -1;
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
